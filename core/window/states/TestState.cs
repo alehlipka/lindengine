37063816a08e1c88b3d54fded41047ef30872f73
Tolik <alehlipka@gmail.com>
2024-02-05 11:03:22 +0300

    GUI refactoring

--- core/window/states/TestState.cs
+++ core/window/states/TestState.cs
@@ -1,5 +1,4 @@
-﻿using lindengine.core.helpers;
-using lindengine.gui;
+﻿using lindengine.gui;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 
@@ -7,44 +6,31 @@ namespace lindengine.core.window.states
 {
     internal class TestState(string name, Vector2i windowSize) : State(name, windowSize)
     {
-        private readonly Text? textElement = new($"{name}_text_element", new Vector2i(windowSize.X, 50), "Test state", 24, Color4.White);
         private readonly Background? background = new($"{name}_background", windowSize, Path.Combine("assets", "backgrounds", "forest.png"));
 
         protected override void OnLoad(State state)
         {
             background?.Load(WindowSize);
-            textElement?.Load(WindowSize);
-
-            FPSCounter.OnFPSChanged += OnFPSChanged;
-        }
-
-        private void OnFPSChanged()
-        {
-            textElement?.SetText($"Test state\nFPS: {FPSCounter.FPS} Max: {FPSCounter.Max} Min: {FPSCounter.Min}");
         }
 
         protected override void OnContextResize(State state, ResizeEventArgs args)
         {
             background?.Resize(args);
-            textElement?.Resize(args);
         }
 
         protected override void OnUpdateFrame(State state, FrameEventArgs args)
         {
             background?.Update(args);
-            textElement?.Update(args);
         }
 
         protected override void OnRenderFrame(State state, FrameEventArgs args)
         {
             background?.Render(args);
-            textElement?.Render(args);
         }
 
         protected override void OnUnload(State state)
         {
             background?.Unload();
-            textElement?.Unload();
         }
     }
 }
--- gui/Element.cs
+++ gui/Element.cs
@@ -1,4 +1,7 @@
-﻿using OpenTK.Mathematics;
+﻿using lindengine.common.cameras;
+using lindengine.common.shaders;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 
 namespace lindengine.gui
@@ -40,8 +43,69 @@ namespace lindengine.gui
                 UnloadEvent += OnUnload;
 
                 LoadEvent?.Invoke(this, windowSize);
+
+                InitializeVertices();
+                InitializeIndices();
+                InitializeBuffers();
+
                 _isLoaded = true;
             }
+        }
+
+        protected virtual void InitializeVertices()
+        {
+            vertices = [
+                0.0f,   0.0f,   0.0f, 0.0f, 0.0f,  // bottom left
+                0.0f,   size.Y, 0.0f, 0.0f, 1.0f,  // top left
+                size.X, size.Y, 0.0f, 1.0f, 1.0f,  // top right
+                size.X, 0.0f,   0.0f, 1.0f, 0.0f,  // bottom right
+            ];
+        }
+
+        protected virtual void InitializeIndices()
+        {
+            indices = [0, 3, 2, 0, 2, 1];
+        }
+
+        protected virtual void InitializeBuffers()
+        {
+            ShaderManager.Select("gui");
+            int position_attribute = ShaderManager.GetAttribLocation("aPosition");
+            int texture_attribute = ShaderManager.GetAttribLocation("aTexture");
+            vertexBuffer = GL.GenBuffer();
+            BindVertexBuffer();
+            indexBuffer = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, indexBuffer);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, sizeof(uint) * indices.Length, indices, BufferUsageHint.StaticDraw);
+            // Generate a name for the array and create it.
+            // Note that glGenVertexArrays() won't work here.
+            GL.CreateVertexArrays(1, out vertexArray);
+            // Instead of binding it, we pass it to the functions below.
+            // Enable my attributes
+            GL.EnableVertexArrayAttrib(vertexArray, position_attribute);
+            GL.EnableVertexArrayAttrib(vertexArray, texture_attribute);
+            // Set up the formats for my attributes
+            GL.VertexArrayAttribFormat(
+                vertexArray,
+                position_attribute,     // attribute index, from the shader location = 1
+                3,                      // size of attribute, vec3
+                VertexAttribType.Float, // contains floats
+                false,                  // does not need to be normalized as it is already, floats ignore this flag anyway
+                0                       // relative offset after a Vector3
+            );
+            GL.VertexArrayAttribFormat(vertexArray, texture_attribute, 2, VertexAttribType.Float, false, 12);
+            // Make my attributes all use binding 0
+            GL.VertexArrayAttribBinding(vertexArray, position_attribute, 0);
+            GL.VertexArrayAttribBinding(vertexArray, texture_attribute, 0);
+            // Quickly bind all attributes to use "buffer"
+            GL.VertexArrayVertexBuffer(vertexArray, 0, vertexBuffer, 0, 20);
+            GL.VertexArrayElementBuffer(vertexArray, indexBuffer);
+        }
+
+        protected virtual void BindVertexBuffer()
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBuffer);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
         }
 
         public void Resize(ResizeEventArgs e)
@@ -65,6 +129,16 @@ namespace lindengine.gui
             if (_isLoaded)
             {
                 RendereFrameEvent?.Invoke(this, args);
+
+                ShaderManager.Select("gui");
+                CameraManager.Select(CameraType.Orthographic);
+
+                GL.BindVertexArray(vertexArray);
+                ShaderManager.SetUniformData("viewMatrix", CameraManager.GetViewMatrix());
+                ShaderManager.SetUniformData("projectionMatrix", CameraManager.GetProjectionMatrix());
+                ShaderManager.SetUniformData("modelMatrix", modelMatrix);
+
+                GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
             }
         }
 
@@ -73,6 +147,12 @@ namespace lindengine.gui
             if (_isLoaded)
             {
                 UnloadEvent?.Invoke(this);
+
+                GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+                GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
+                GL.DeleteBuffer(indexBuffer);
+                GL.DeleteBuffer(vertexBuffer);
+                GL.DeleteVertexArray(vertexArray);
 
                 LoadEvent -= OnLoad;
                 ContextResizeEvent -= OnContextResize;
